3926d026a5356ff68ade13ccf982a7c0292538d0
Aditya Mukherjee <mukherjee.aditya.official@gmail.com>
2022-12-13 04:28:02 +0530

    Added body-Parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig.js');
 
 const setupAndstartServer = async function () {
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
 
   app.listen(PORT, function () {
     console.log("Server started at", PORT);
