00f61da5b0d7613f2e0e1d6039dc1643df5ac74c
Aditya Mukherjee <mukherjee.aditya.official@gmail.com>
2022-12-18 01:26:09 +0530

    Added routes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig.js');
 
-const CityRepository = require("./repository/city-repository");
+const ApiRoutes = require('./routes/index');
 
 const setupAndstartServer = async function () {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
+
+  app.use('/api/', ApiRoutes);
 
   app.listen(PORT, async function () {
     console.log("Server started at", PORT);
