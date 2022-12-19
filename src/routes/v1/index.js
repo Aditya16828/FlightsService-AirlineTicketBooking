c108723a24d7ebd869b6209dbe8eec833c483739
Aditya Mukherjee <mukherjee.aditya.official@gmail.com>
2022-12-20 01:50:07 +0530

    Setup CRUD API for Airports

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
-const CityController = require('../../controllers/city-controller.js');
+const CityController = require('../../controllers/city-controller');
+const AirportController = require('../../controllers/airport-controller');
 
 const router = express.Router();
 
 router.post('/city', CityController.create); // CREATE
+router.post('/cities', CityController.createCities);
 router.get('/city/:id', CityController.get); // READ
+router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update); // UPDATE
 router.delete('/city/:id', CityController.destroy); // DELETE
 
-router.get('/city', CityController.getAll);
+
+router.post('/city/airports/:cityid', AirportController.create); // (creating airports/airport)
+router.get('/city/airports/:cityid', AirportController.get); // (Read all the airports for a city)
+router.get('/airport/:id', AirportController.getAirport); // (Read a particular airport for a city)
+router.delete('/airport/:id', AirportController.deleteAirport); // (update a particular airport)
+router.patch('/airport/:id', AirportController.updateAirport); // (delete a particular airport)
+
 
 module.exports = router;
